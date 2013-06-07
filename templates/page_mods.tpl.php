d236ec8ca43518dbb34dd65b3db433e46e953078
Jacob Sanford <jsanford@unb.ca>
2013-06-06 22:36:09 -0300

    Another template shuffle to match script from Donald

---
+++ templates/page_mods.tpl.php
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<mods xmlns="http://www.loc.gov/mods/v3" xmlns:mods="http://www.loc.gov/mods/v3" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<titleInfo>
+<title>Page {$page_number_short}</title>
+</titleInfo>
+    <part>
+        <extent unit="pages">
+            <start>{$sequence_number}</start>
+        </extent>
+        <detail type="page number">
+            <number>{$page_number_short}</number>
+        </detail>
+    </part>
+    <relatedItem type="original">
+        <physicalDescription>
+            <form type="{$source_media}"/>
+        </physicalDescription>
+        <identifier type="reel number">{$reel_number}</identifier>
+        <identifier type="reel sequence number">{$sequence_number}</identifier>
+        <location>
+            <physicalLocation authority="marcorg" displayLabel="Source Repository">{$marcorg_id}</physicalLocation>
+        </location>
+    </relatedItem>
+    <identifier type="PID">{$pid}</identifier>
+    <note type="agencyResponsibleForReproduction" displayLabel="Institution Responsible for Digitization">{$marcorg_id}</note>
+</mods>
---
+++ templates/page_rdf.tpl.php
@@ -0,0 +1,10 @@
+<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:fedora="info:fedora/fedora-system:def/relations-external#" xmlns:fedora-model="info:fedora/fedora-system:def/model#" xmlns:islandora="http://islandora.ca/ontology/relsext#">
+<rdf:Description rdf:about="info:fedora/$pid">
+        <fedora-model:hasModel xmlns="info:fedora/fedora-system:def/model#" rdf:resource="info:fedora/islandora:pageCModel"></fedora-model:hasModel>
+        <fedora:isSequenceNumber xmlns="http://islandora.ca/ontology/relsext#">$page_number_short</fedora:isSequenceNumber>
+        <fedora:isMemberOf xmlns="info:fedora/fedora-system:def/relations-external#" rdf:resource="info:fedora/$collection_pid"></fedora:isMemberOf>
+        <fedora:isSection xmlns="http://islandora.ca/ontology/relsext#">1</fedora:isSection>
+        <fedora:isPageOf xmlns="http://islandora.ca/ontology/relsext#" rdf:resource="inf:fedora/$collection_pid"></fedora:isPageOf>
+        <fedora:isPageNumber xmlns="http://islandora.ca/ontology/relsext#">$page_number_short</fedora:isPageNumber>
+</rdf:Description>
+</rdf:RDF>
