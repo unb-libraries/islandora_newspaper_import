d085ddb055d167b2553137a5c736d284de1e8467
Jacob Sanford <jsanford@unb.ca>
2013-10-24 16:37:25 -0300

    Move issue and page title info in MODS to <subtitle> elements to allow faceting on Newspaper Name

--- templates/page_mods.tpl.php
+++ templates/page_mods.tpl.php
@@ -1,7 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <mods xmlns="http://www.loc.gov/mods/v3" xmlns:mods="http://www.loc.gov/mods/v3" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
 <titleInfo>
-<title>Page {$page_number_short}</title>
+<title/>
+<subTitle>Page {$page_number_short}</subTitle>
 </titleInfo>
     <part>
         <extent unit="pages">
