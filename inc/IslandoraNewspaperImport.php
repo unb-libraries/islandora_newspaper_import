92ccd64836d39760fec84a04baca95a03853a27f
Jacob Sanford <jsanford@unb.ca>
2013-12-11 08:20:52 -0400

    Finish cleanup of IslandoraNewspaperImport. Code standards. Issue #4.

--- inc/IslandoraNewspaperImport.php
+++ inc/IslandoraNewspaperImport.php
@@ -34,7 +34,7 @@ class IslandoraNewspaperImport {
    * Without ReflectionClass it seems unlikely we can obtain the full, absolute
    * path to the drush command source tree.
    */
-  public function assignTemplatePath() {
+  private function assignTemplatePath() {
     $reflector = new ReflectionClass(get_class($this));
     $file_name = $reflector->getFileName();
     $this->templatePath = dirname($file_name) . '/../templates';
@@ -43,7 +43,7 @@ class IslandoraNewspaperImport {
   /**
    * Initialize and assign the Fedora repository object property and API.
    */
-  public function fedoraInit($fedora_url, $fedora_user, $fedora_password) {
+  private function fedoraInit($fedora_url, $fedora_user, $fedora_password) {
     $this->connection = new RepositoryConnection($fedora_url, $fedora_user, $fedora_password);
     $this->api = new FedoraApi($this->connection);
     $cache = new SimpleCache();
@@ -57,117 +57,169 @@ class IslandoraNewspaperImport {
       die("Cannot ingest or purge items from random pid $random_pid (Check URL/credentials)\n");
     }
   }
-
-  function buildIssue($marcOrgID, $parentCollectionPID, $nameSpace, $special_identifier) {
+  /**
+   * Constructs the IslandoraNewspaperIssue object and datastreams.
+   */
+  protected function buildIssue($marcorg_id, $parent_pid, $issue_namespace, $special_identifier) {
     $this->setupIssueSourceData();
     $this->validateIssueConfigData();
-    $this->issue=new IslandoraNewspaperImportIssue($this->api, SOURCE_MEDIA, $marcOrgID, ISSUE_CONTENT_MODEL_PID, $nameSpace, $parentCollectionPID, ISSUE_TITLE, ISSUE_LCCN, ISSUE_DATE, ISSUE_VOLUME, ISSUE_ISSUE, ISSUE_EDITION, MISSING_PAGES, ISSUE_LANGUAGE, $special_identifier, ISSUE_SUPPLEMENT_TITLE, ISSUE_ERRATA);
-    $this->issue->createContent($this->imagesToImport, PAGE_CONTENT_MODEL_PID, $templatePath, $xslPath);
+    $this->issue = new IslandoraNewspaperImportIssue(
+            $this->api,
+            SOURCE_MEDIA,
+            $marcorg_id,
+            ISSUE_CONTENT_MODEL_PID,
+            $issue_namespace,
+            $parent_pid,
+            ISSUE_TITLE,
+            ISSUE_LCCN,
+            ISSUE_DATE,
+            ISSUE_VOLUME,
+            ISSUE_ISSUE,
+            ISSUE_EDITION,
+            MISSING_PAGES,
+            ISSUE_LANGUAGE,
+            $special_identifier,
+            ISSUE_SUPPLEMENT_TITLE,
+            ISSUE_ERRATA
+            );
+    $this->issue->createContent(
+            $this->imagesToImport,
+            PAGE_CONTENT_MODEL_PID,
+            $this->templatePath,
+            $this->xslPath
+            );
   }
-
-  function ingestIssue() {
+  /**
+   * Ingests the newspaper issue object into the Fedora repository.
+   */
+  protected function ingestIssue() {
     $this->issue->ingest($this->repository);
   }
-
-  function buildTitle($title_string, $collection_pid, $title_pid) {
+  /**
+   * Builds the newspaper title Fedora object and datastreams.
+   */
+  protected function buildTitle($title_string, $collection_pid, $title_pid) {
     $this->collectionPID = $collection_pid;
     $this->titlePID = $title_pid;
     $this->titleTitle = $title_string;
     $this->assignTemplatePath();
     $this->assignXSLPath();
 
-    // Generate RDF
-    $titleRDF= new Smarty;
-    $titleRDF->assign('title_pid', $this->titlePID);
-    $titleRDF->assign('collection_pid', $this->collectionPID);
+    $title_rdf_smarty = new Smarty();
+    $title_rdf_smarty->assign('title_pid', $this->titlePID);
+    $title_rdf_smarty->assign('collection_pid', $this->collectionPID);
     $this->xml['RDF'] = new DOMDocument();
-    $this->xml['RDF']->loadXML($titleRDF->fetch(join('/', array($this->templatePath, 'title_rdf.tpl.php'))));
+    $this->xml['RDF']->loadXML($title_rdf_smarty->fetch(implode('/', array($this->templatePath, 'title_rdf.tpl.php'))));
 
-    // Generate MODS
     $this->xml['MODS'] = new DOMDocument();
-    $this->xml['MODS']->loadXML(file_get_contents(join('/', array($this->importPath,'MODS.xml'))));
+    $this->xml['MODS']->loadXML(file_get_contents(implode('/', array($this->importPath, 'MODS.xml'))));
 
-    // Generate DC
-    $transformXSL = new DOMDocument();
-    $transformXSL->load(join('/', array($this->xslPath, 'mods_to_dc.xsl')));
+    $mods_dc_transform = new DOMDocument();
+    $mods_dc_transform->load(implode('/', array($this->xslPath, 'mods_to_dc.xsl')));
     $processor = new XSLTProcessor();
-    $processor->importStylesheet($transformXSL);
+    $processor->importStylesheet($mods_dc_transform);
     $this->xml['DC'] = new DOMDocument();
     $this->xml['DC']->loadXML($processor->transformToXML($this->xml['MODS']));
   }
+  /**
+   * Ingests the newspaper title object into the Fedora repository.
+   */
+  protected function ingestTitle() {
+    $title_fedora_object = new NewFedoraObject($this->titlePID, $this->repository);
+    $title_fedora_object->label = $this->titleTitle;
 
-  function ingestTitle() {
-    $titleObjectToIngest = new NewFedoraObject($this->titlePID, $this->repository);
-    $titleObjectToIngest->label = $this->titleTitle;
+    $title_ds_rdf = new NewFedoraDatastream('RELS-EXT', 'X', $title_fedora_object, $this->repository);
+    $title_ds_rdf->content = $this->xml['RDF']->saveXML();
+    $title_ds_rdf->mimetype = 'application/rdf+xml';
+    $title_ds_rdf->label = 'Fedora Object to Object Relationship Metadata.';
+    $title_ds_rdf->logMessage = 'RELS-EXT datastream created using Newspapers batch ingest script || SUCCESS';
+    $title_fedora_object->ingestDatastream($title_ds_rdf);
 
-    // Add RDF
-    $titleDSRDF = new NewFedoraDatastream('RELS-EXT', 'X', $titleObjectToIngest, $this->repository);
-    $titleDSRDF->content = $this->xml['RDF']->saveXML();
-    $titleDSRDF->mimetype = 'application/rdf+xml';
-    $titleDSRDF->label = 'Fedora Object to Object Relationship Metadata.';
-    $titleDSRDF->logMessage = 'RELS-EXT datastream created using Newspapers batch ingest script || SUCCESS';
-    $titleObjectToIngest->ingestDatastream($titleDSRDF);
+    $title_ds_mods = new NewFedoraDatastream('MODS', 'M', $title_fedora_object, $this->repository);
+    $title_ds_mods->content = $this->xml['MODS']->saveXML();
+    $title_ds_mods->mimetype = 'text/xml';
+    $title_ds_mods->label = 'MODS Record';
+    $title_ds_mods->checksum = TRUE;
+    $title_ds_mods->checksumType = 'MD5';
+    $title_ds_mods->logMessage = 'Title MODS datastream created using Newspapers batch ingest script || SUCCESS';
+    $title_fedora_object->ingestDatastream($title_ds_mods);
 
-    // Add MODS
-    $titleDSMODS = new NewFedoraDatastream('MODS', 'M', $titleObjectToIngest, $this->repository);
-    $titleDSMODS->content = $this->xml['MODS']->saveXML();
-    $titleDSMODS->mimetype = 'text/xml';
-    $titleDSMODS->label = 'MODS Record';
-    $titleDSMODS->checksum = TRUE;
-    $titleDSMODS->checksumType = 'MD5';
-    $titleDSMODS->logMessage = 'Title MODS datastream created using Newspapers batch ingest script || SUCCESS';
-    $titleObjectToIngest->ingestDatastream($titleDSMODS);
+    $title_ds_dc = new NewFedoraDatastream('DC', 'X', $title_fedora_object, $this->repository);
+    $title_ds_dc->content = $this->xml['DC']->saveXML();
+    $title_ds_dc->mimetype = 'text/xml';
+    $title_ds_dc->label = 'DC Record';
+    $title_ds_dc->logMessage = 'DC datastream created using Newspapers batch ingest script || SUCCESS';
+    $title_fedora_object->ingestDatastream($title_ds_dc);
 
-    // Add DC
-    $titleDSDC = new NewFedoraDatastream('DC', 'X', $titleObjectToIngest, $this->repository);
-    $titleDSDC->content = $this->xml['DC']->saveXML();
-    $titleDSDC->mimetype = 'text/xml';
-    $titleDSDC->label = 'DC Record';
-    $titleDSDC->logMessage = 'DC datastream created using Newspapers batch ingest script || SUCCESS';
-    $titleObjectToIngest->ingestDatastream($titleDSDC);
-
-    $this->repository->ingestObject($titleObjectToIngest);
-    // TODO: Islandora newspapercmodel hook to fire?
+    $this->repository->ingestObject($title_fedora_object);
+    // @todo: Is there an Islandora islandora:newspaperPageCModel post ingest
+    // notification hook to fire? It seems to have been phased out from
+    // previous versions.
   }
-
-  function setupIssueSourceData() {
-    $importPath = $this->importPath;
-    if (!$importPath) {
-      $this->import_path=drush_prompt(dt('Path to Import Directory'), NULL, TRUE);
-    } else {
-      $this->import_path=$importPath;
+  /**
+   * Traverses $this->importPath to determine which images to ingest as pages.
+   */
+  private function setupIssueSourceData() {
+    $import_path = $this->importPath;
+    if (!$import_path) {
+      $this->import_path = drush_prompt(dt('Path to Import Directory'), NULL, TRUE);
     }
-    $this->imagesToImport=array();
-    $file_list=glob(join('/',array($this->import_path,'*.[jJ][pP][gG]')));
-    foreach ($file_list as $filePathToImport) {
-      $this->imagesToImport[]=array(
-                'pageno' => str_replace('.jpg','', array_pop(explode('_', basename($filePathToImport)))),
-                'filepath' => $filePathToImport
-                );
+    else {
+      $this->import_path = $import_path;
+    }
+    $this->imagesToImport = array();
+    $file_list = glob(implode('/', array($this->import_path, '*.[jJ][pP][gG]')));
+    foreach ($file_list as $filepath_of_image) {
+      $this->imagesToImport[] = array(
+        'pageno' => str_replace(
+                '.jpg',
+                '',
+                array_pop(
+                        explode(
+                                '_',
+                                basename($filepath_of_image)
+                                )
+                        )
+        ),
+        'filepath' => $filepath_of_image,
+      );
     }
     $this->validateSourcePath();
   }
-
-  function validateIssueConfigData() {
+  /**
+   * Validates required metadata elements from metadata.php file.
+   */
+  private function validateIssueConfigData() {
     $this->validateIssueDate();
-    $must_exist_data=array(
-        'ISSUE_TITLE',
-        'ISSUE_MEDIA',
-        'ISSUE_VOLUME',
-        'ISSUE_ISSUE',
-        # 'ISSUE_EDITION',
-        );
-    foreach ($must_exist_data as $cur_data) if (!constant($cur_data) || constant($cur_data) == '') die ("$cur_data is not set in metadata.php\n");
+    $must_exist_data = array(
+      'ISSUE_TITLE',
+      'ISSUE_MEDIA',
+      'ISSUE_VOLUME',
+      'ISSUE_ISSUE',
+    );
+    foreach ($must_exist_data as $cur_data) {
+      if (!constant($cur_data) || constant($cur_data) == '') {
+        die ("$cur_data is not set in metadata.php\n");
+      }
+    }
   }
-
-  function validateIssueDate(){
-    if (!checkdate(date('m',ISSUE_DATE), date('d',ISSUE_DATE), date('Y',ISSUE_DATE))) {
+  /**
+   * Validates const ISSUE_DATE.
+   */
+  private function validateIssueDate() {
+    if (!checkdate(date('m', ISSUE_DATE), date('d', ISSUE_DATE), date('Y', ISSUE_DATE))) {
       die("Date from metadata [{ISSUE_DATE}]is invalid\n");
     }
   }
-
-  function validateSourcePath() {
-    if(!@include(join('/', array($this->import_path,'metadata.php')))) die("Failed to load 'metadata.php' from {$this->import_path}\n");
-    if (sizeof($this->imagesToImport) < 1 ) die("No *.jpg files exist in import path {$this->import_path}\n");
+  /**
+   * Validates $this->import_path to ensure that metadata.php and images exist.
+   */
+  private function validateSourcePath() {
+    if (!@include implode('/', array($this->import_path, 'metadata.php'))) {
+      die("Failed to load 'metadata.php' from {$this->import_path}\n");
+    }
+    if (count($this->imagesToImport) < 1) {
+      die("No *.jpg files exist in import path {$this->import_path}\n");
+    }
   }
 }
